833caad27dd8c337cf2c41ab8ddad8f94226aa36
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-05-29 18:25:47 -0500

    build: mysql

--- app/pool.ts
+++ app/pool.ts
@@ -1,10 +1,13 @@
-import { Pool } from 'pg';
+import mysql from 'mysql2/promise';
 
-const pool = new Pool({
-  host: 'postgres',
+const pool = mysql.createPool({
+  host: 'mysql',
   user: 'user',
   password: 'password',
   database: 'app_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default pool;
